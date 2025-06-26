958c1beaf217dc8770e05eb1eeb0138ebd32594d
VieiraEnzo <vieira.enzof@gmail.com>
2025-06-26 13:48:48 -0300

    Modelo geral

---
+++ K-means.h
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <functional>
+#include "Point.h"
+using namespace std;
+
+
+struct KMEANS{
+
+    int n;
+    vector<Point> pts;
+
+    KMEANS(vector<Point> pts) : pts(pts), n(pts.size())
+    {
+  
+    }
+
+
+    /*
+        Objetivo: 
+        Complexidade:  
+    */
+    int run(){
+
+    }
+
+};
---
+++ Kohonen.h
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <functional>
+#include "Point.h"
+using namespace std;
+
+
+
+struct Kohonen{
+
+    int n;
+    vector<Point> pts;
+
+    Kohonen(vector<Point> pts) : pts(pts), n(pts.size())
+    {
+  
+    }
+
+
+    /*
+        Objetivo: 
+        Complexidade:  
+    */
+    int run(){
+
+    }
+
+};
